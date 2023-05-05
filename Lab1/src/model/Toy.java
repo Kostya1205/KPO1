c79a0e293783de0503b93602fcdea44f570c6c3f
kostya1205 <kostya.zhurovich12052@gmail.com>
2023-05-06 00:39:00 +0300

    Add a copy constructor

--- Lab1/src/model/Toy.java
+++ Lab1/src/model/Toy.java
@@ -10,7 +10,22 @@ public class Toy {
         this.weight = weight;
         this.name = name;
     }
+    public Toy(String name) {
+        this.name = name;
+        this.weight = 14;
+        this.price = 5;
+    }
 
+    public Toy(int price) {
+        this.name = "Kostya";
+        this.price = price;
+        this.weight = 14;
+    }
+    public Toy(Toy toy) {
+        this.name = toy.getName();
+        this.price = 1;
+        this.weight = 14;
+    }
     public int getPrice() {
         return price;
     }
